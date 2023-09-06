4520bd2732b41c04e77d1bab50eb9a723d877088
vandbaba <124664881+vandbaba@users.noreply.github.com>
2023-09-06 09:48:41 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World pritam jenkins!" );
+        System.out.println( "Hello hi are you,gd morning!" );
     }
 }
